747c135a4fedf226bb4343619638ac52e803c4f7
shawn_he <shawn.he@huawei.com>
2022-02-21 17:16:16 +0800

    update docs
    Signed-off-by: shawn_he <shawn.he@huawei.com>

--- interfaces/kits/js/@ohos.thermal.d.ts
+++ interfaces/kits/js/@ohos.thermal.d.ts
@@ -16,11 +16,12 @@
 import { AsyncCallback, Callback } from './basic';
 
 /**
- * Provides thermal level-related callback and query APIs to obtain the information required for temperature control. The APIs are as follows:
+ * Provides thermal level-related callback and query APIs to obtain the information required for 
+ * temperature control. The APIs are as follows:
  * {@link subscribeThermalLevel}: subscribes to callbacks of thermal level changes.
  * {@link getThermalLevel}: obtains the thermal level of the system in real time.
  *
- * @SysCap SystemCapability.PowerMgr.ThermalManager
+ * @SysCap SystemCapability.PowerManager.ThermalManager
  * @since 8
  */
 declare namespace thermal {
@@ -43,19 +44,23 @@ declare namespace thermal {
          */
         WARM = 2,
         /**
-         * The device is heating up. You need to stop all imperceptible services and downgrade or reduce the load of other services.
+         * The device is heating up. You need to stop all imperceptible services and downgrade 
+         * or reduce the load of other services.
          */
         HOT = 3,
         /**
-         * The device is overheated. You need to stop all imperceptible services and downgrade or reduce the load of major services.
+         * The device is overheated. You need to stop all imperceptible services and downgrade 
+         * or reduce the load of major services.
          */
         OVERHEATED = 4,
         /**
-         * The device is overheated and is about to enter the emergency state. You need to stop all imperceptible services and downgrade major services to the maximum extent.
+         * The device is overheated and is about to enter the emergency state. You need to stop 
+         * all imperceptible services and downgrade major services to the maximum extent.
          */
         WARNING = 5,
         /**
-         * The device has entered the emergency state. You need to stop all services except those for the emergency help purposes.
+         * The device has entered the emergency state. You need to stop all services except those 
+         * for the emergency help purposes.
          */
         EMERGENCY = 6,
     }
