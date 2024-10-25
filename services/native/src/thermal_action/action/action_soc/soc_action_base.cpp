de67d5af7eca996d09f4695252078ee56140d59d
youshugen <youshugen@huawei.com>
2024-10-25 13:41:56 +0800

    fix: alarming improve
    Signed-off-by: youshugen <youshugen@huawei.com>

--- services/native/src/thermal_action/action/action_soc/soc_action_base.cpp
+++ services/native/src/thermal_action/action/action_soc/soc_action_base.cpp
@@ -25,6 +25,7 @@ namespace {
 #ifdef SOC_PERF_ENABLE
 constexpr int32_t ACTION_TYPE_THERMAL_ID = 2;
 constexpr int32_t ACTION_TYPE_CPU_ISOLATE_CMDID = 12361;
+const std::string THERMAL_MSG = "thermal";
 #endif
 }
 
@@ -50,6 +51,13 @@ void SocActionBase::SocIsolateRequest(bool enable)
 {
 #ifdef SOC_PERF_ENABLE
     OHOS::SOCPERF::SocPerfClient::GetInstance().PerfRequestEx(ACTION_TYPE_CPU_ISOLATE_CMDID, enable, "");
+#endif
+}
+
+void SocActionBase::SetBoostEnable(bool status)
+{
+#ifdef SOC_PERF_ENABLE
+    OHOS::SOCPERF::SocPerfClient::GetInstance().SetRequestStatus(status, THERMAL_MSG);
 #endif
 }
 } // namespace PowerMgr
