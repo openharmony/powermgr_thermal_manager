0c9c4d9158ae7465c9adb03075d29e35f784573c
wrobby <wangjincheng16@huawei.com>
2024-12-06 06:29:57 +0000

    feat: add state collection,charge_delay and startup_delay
    Signed-off-by: wrobby <wangjincheng16@huawei.com>

--- services/native/include/thermal_observer/state_machine/charge_delay_state_collection.h
+++ services/native/include/thermal_observer/state_machine/charge_delay_state_collection.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +21,6 @@
 #include "thermal_mgr_listener.h"
 #include "thermal_timer.h"
 
-using IntentWant = OHOS::AAFwk::Want;
 namespace OHOS {
 namespace PowerMgr {
 class ChargeDelayStateCollection : public IStateCollection {
@@ -40,7 +39,7 @@ public:
     void HandlerPowerConnected(const EventFwk::CommonEventData& data);
     bool RegisterEvent();
     bool StartDelayTimer();
-    void StopDelayAction();
+    void StopDelayTimer();
     void ResetState();
     bool InitDelayTime(std::string& delaytime) override;
     virtual void SetState(const std::string& stateValue) override;
