d32d7fab6e1c114335640f7b32b1c6fbbba118cd
jiangyang <2414597505@qq.com>
2025-03-10 15:56:56 +0800

    fix: add nonvip action Signed-off-by: jiangyang <2414597505@qq.com>

--- services/native/src/thermal_action/action/action_soc/action_cpu_nonvip.cpp
+++ services/native/src/thermal_action/action/action_soc/action_cpu_nonvip.cpp
@@ -45,7 +45,7 @@ void ActionCpuNonVip::SetEnableEvent(bool enable)
     enableEvent_ = enable;
 }
 
-void ActionCpuNonVip::AddActionValue(uint32_t actionId, std::string value)
+void ::AddActionValue(uint32_t actionId, std::string value)
 {
     if (value.empty()) {
         return;
@@ -54,10 +54,12 @@ void ActionCpuNonVip::AddActionValue(uint32_t actionId, std::string value)
     if (actionId > 0) {
         auto iter = policyActionMap_.find(actionId);
         if (iter != policyActionMap_.end()) {
-            iter->second.uintDelayValue = static_cast<uint32_t>(std::stoul(value));
+            iter->second.uintDelayValue = static_cast<uint32_t>(strtol(value.c_str(),
+                nullptr, STRTOL_FORMART_DEC));
         }
     } else {
-        valueList_.push_back(static_cast<uint32_t>(std::stoul(value)));
+        valueList_.push_back(static_cast<uint32_t>(strtol(value.c_str(),
+                nullptr, STRTOL_FORMART_DEC)));
     }
 }
 
